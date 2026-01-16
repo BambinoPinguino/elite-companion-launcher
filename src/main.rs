fa30295be0f6788e15c45440c913c810f4cc945f
Kyle <ababypenguin@gmail.com>
2026-01-15 23:09:12 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io::{self, Write};
+use std::process::Command;
+
+fn listdir() {
+    let list_dir = Command::new("bash")
+        .arg("-c")
+        .arg("ls")
+        .arg("-lah")
+        .output()
+        .expect("failed to execute process");
+
+    let _ = io::stdout().write(&list_dir.stdout);
+}
+fn main() {
+    println!("Hello World!");
+}
